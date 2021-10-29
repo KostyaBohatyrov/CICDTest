f83cb1c8bcdebe486cd663bb91d2e85a1f2b43b7
Andrew Yehozha <andrew.yehozha@tesseris.com>
2021-10-29 12:16:08 +0300

    Add project files.

---
+++ CICDTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CICDTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
