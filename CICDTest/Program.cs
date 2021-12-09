df79fece1d175d0674e3d49123a9cc7abdbf004f
Andrew Yehozha <andrew.yehozha@tesseris.com>
2021-12-09 11:18:48 +0200

    2

--- CICDTest/Program.cs
+++ CICDTest/Program.cs
@@ -9,8 +9,6 @@ namespace CICDTest
             Console.WriteLine("Start");
             Console.WriteLine("Hello World!");
             Console.WriteLine("End");
-            Console.WriteLine("dev1");
-            Console.WriteLine("To merge on dev");
         }
     }
 }
