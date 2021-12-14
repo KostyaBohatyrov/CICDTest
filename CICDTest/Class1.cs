a99b91082bcccebe89db85ffd49b8ec8d868b32a
Andrew Yehozha <andrew.yehozha@tesseris.com>
2021-12-14 14:51:43 +0200

    add prop to class1

--- CICDTest/Class1.cs
+++ CICDTest/Class1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CICDTest
+﻿namespace CICDTest
 {
     class Class1
     {
+        public int Counter { get; set; }
     }
 }
