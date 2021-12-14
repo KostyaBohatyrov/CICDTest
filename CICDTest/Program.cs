0796942547adf7e0e6c46a53fde1171e8cb3754d
Andrew Yehozha <andrew.yehozha@tesseris.com>
2021-12-14 14:52:24 +0200

    Add cw to main

--- CICDTest/Program.cs
+++ CICDTest/Program.cs
@@ -9,6 +9,7 @@ namespace CICDTest
             Console.WriteLine("Start");
             Console.WriteLine("Helqqo Wofbsstt2cr2ad!");
             Console.WriteLine("Enddd2edsvafd245336zsvzasda456456sdsssssetsdfsdfeeawdwadtretraasa46645rssssyyytag-46zsvww2");
+            Console.WriteLine("Add cw to main");
         }
     }
 }
