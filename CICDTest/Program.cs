4f1db5869f23fea7a8def8c796f43206a4644683
Andrew Yehozha <andrew.yehozha@tesseris.com>
2021-12-14 15:10:34 +0200

    add sayHello method

--- CICDTest/Program.cs
+++ CICDTest/Program.cs
@@ -9,5 +9,10 @@ namespace CICDTest
             Console.WriteLine("Add cw to main");
             Console.WriteLine("Add one more cw to main");
         }
+
+        private void sayHello()
+        {
+
+        }
     }
 }
